8bce31997b65ebb805e71f06a424ded13b6b86f6
kawagh <kawagh@example.com>
2022-04-06 18:58:32 +0900

    passed simple test case

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+fn shunting_yard(input: &str) -> Vec<char> {
+    let mut operator_stack: Vec<char> = Vec::new();
+    let mut rpn_stack: Vec<char> = Vec::new();
+    for ch in input.chars() {
+        match ch {
+            '+' => operator_stack.push(ch),
+            num => rpn_stack.push(num),
+        }
+    }
+    while let Some(ch) = operator_stack.pop() {
+        rpn_stack.push(ch);
+    }
+    rpn_stack
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = "1+2";
+    let output = shunting_yard(input);
+    println!("input: {:?}", input);
+    println!("output: {:?}", output);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::shunting_yard;
+    #[test]
+    fn one_char_case() {
+        let input = "1+2";
+        assert_eq!(shunting_yard(input), vec!['1', '2', '+']);
+    }
+
+    #[test]
+    fn one_char_case2() {
+        let input = "1+2+3";
+        assert_eq!(shunting_yard(input), vec!['1', '2', '3', '+', '+']);
+    }
 }
